2af9d1b8552d78b028492658accf9cd717d1fec4
Przemysław Bryk <34355073+przempb@users.noreply.github.com>
2021-08-20 14:42:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from google_interface import Drive
 import pandas as pd
 
 # The ID and range of a spreadsheet you want to read.
-SAMPLE_SPREADSHEET_ID_GOOGLE = '1IoK_D6YgaQ6lat3b7KK4LbiE7fNyUgFy1BSzU0v9txA'
+SAMPLE_SPREADSHEET_ID_GOOGLE = '1IoK_D6YgaQ6lasadasdLbiE7fNyUgFy1BSzU0v9txA'
 SAMPLE_RANGE = 'A:D'
 #...or update
-SPREADSHEET_TO_UPDATE = '1nBXuCTHT5JQ8Es7aN4Rd6DZTZnifCgJP0tBczo0Pwi8'
+SPREADSHEET_TO_UPDATE = '1nBXuCTHTfsdfsdfsd4Rd6DZTZnifCgJP0tBczo0Pwi8'
 
 
 g_drive = Drive()
